e68f874e7657c8b826d7a2ec72cf1e1069f412d8
Ferhat Guemues <ferhat_gumus@teilnehmer.cdemy.de>
2025-10-01 12:40:04 +0200

    .

--- main.dart
+++ main.dart
@@ -1,35 +1,16 @@
-import 'dart:math';
-
-import 'teilnehmer.dart';
+import 'AccessIdGenerator.dart';
 
 void main() {
-  Set <int> participantsId={};
-
-
-  final participant1 = Participant(
-    lastname: "der dreckige",
-    firstname: "Dan",
-    gender: Gender.male,
-    id: randomizer(participantsId)
-  );
-  final participant2 = Participant(
-    lastname: "der saubere",
-    firstname: "Dan",
-    gender: Gender.male,
-    id: randomizer(participantsId)
-  );
-final myCourse = Course('Lernsituation', [participant1, participant2]);
-print(myCourse.participants[0].id);
-print(myCourse.participants[1].id);
-print(participantsId);
-
+  AccessIdGenerator.initialize({});
+  List<Participant> participants = [];
+  for (var i = 0; i < 11; i++) {
+    final newParticipant = Participant(
+      lastname: "der dreckige",
+      firstname: "Dan",
+      gender: Gender.male,
+      accessId: AccessIdGenerator.genNewId(),
+    );
+    participants.add(newParticipant);
+    print(newParticipant.accessId);
+  }
 }
-
-int randomizer(Set <int> participantsId){
-  int id=0;
-  do id = Random().nextInt(100000)+100000; 
-  while (participantsId.contains(id));
-  participantsId.add(id);
-  return id;
-}
-
--- teilnehmer.dart
+++ teilnehmer.dart
@@ -1,26 +1,48 @@
+part of 'AccessIdGenerator.dart';
+
 enum Gender {
   female('Female'),
   male('Male'),
   diverse('Diverse'),
   skip('No answer');
-  
+
   final String label;
   const Gender(this.label);
 }
-
 
 class Participant {
   final String lastname;
   final String firstname;
   final Gender gender;
-  final int id;
-
+  final AccessId accessId;
   Participant({
     required this.lastname,
     required this.firstname,
     required this.gender,
-    required this.id
+    required this.accessId,
   });
+}
+
+class AccessId {
+  int get accessId => _accessId;
+  final int _accessId;
+
+  AccessId._(this._accessId);
+
+  @override
+  bool operator ==(Object other) {
+    if (other is! AccessId) return false;
+    if (_accessId != other._accessId) return false;
+    return true;
+  }
+
+  @override
+  int get hashCode => _accessId.hashCode;
+
+  @override
+  String toString() {
+    return "AccessId($_accessId)";
+  }
 }
 
 class Course {
